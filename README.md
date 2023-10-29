9268aca29a304e8b0bfe79e6abb4e307943d0d3c
Pedro Felgueiras <jpfelgueiras@gmail.com>
2023-10-29 18:00:07 +0000

    Correct methods

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # Run postgres docker 
 
-```
+```shell
 docker run --rm -it -e POSTGRES_PASSWORD=postgres -p 5432:5432 postgres:alpine
+cd infrastructure
+diesel setup
 ```
 
 # Install libpq-dev and pgclient
-```
+```shell
 sudo apt install libpq-dev postgresql-client
+```
+
+# Step to run
+
+```shell
+cargo watch -x run
 ```
 
 # Tutorial URL 
--- api/src/post_handler.rs
+++ api/src/post_handler.rs
@@ -3,7 +3,7 @@
 use shared::response_models::{Response, ResponseBody};
 use application::post::{create, read, publish, delete}; 
 use domain::models::{Post, NewPost};  
-use rocket::{get, post}; 
+use rocket::{get, post, put, delete}; 
 use rocket::response::status::{NotFound, Created}; 
 use rocket::serde::json::Json;
 
@@ -11,7 +11,7 @@ use revolt_rocket_okapi::openapi;
 
 
 #[openapi]
-#[get("/")]
+#[get("/post")]
 pub fn list_posts_handler() -> String {
     let posts: Vec<Post> = read::list_posts();
     let response = Response { body: ResponseBody::Posts(posts) };
@@ -29,13 +29,13 @@ pub fn list_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
 }
 
 #[openapi]
-#[post("/new_post", format = "application/json", data = "<post>")]
+#[post("/post", format = "application/json", data = "<post>")]
 pub fn create_post_handler(post: Json<NewPost>) -> Created<String> {
     create::create_post(post)
 }
 
 #[openapi]
-#[get("/publish/<post_id>")]
+#[put("/post/<post_id>/publish")]
 pub fn publish_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
     let post = publish::publish_post(post_id)?; 
     let response = Response { body: ResponseBody::Post(post) };
@@ -44,7 +44,7 @@ pub fn publish_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
 }
 
 #[openapi]
-#[get("/delete/<post_id>")]
+#[delete("/post/<post_id>")]
 pub fn delete_post_handler(post_id: i32) -> Result<String, NotFound<String>> {
     let posts = delete::delete_post(post_id)?;
     let response = Response { body: ResponseBody::Posts(posts) };
